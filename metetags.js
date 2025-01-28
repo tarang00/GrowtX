1acad4b7810a6b7e9a14c5005259f9d011702b12
tarang00 <taranglilhare@gmail.com>
2025-01-28 23:19:58 +0530

    Add all any files

---
+++ metetags.js
@@ -0,0 +1,31 @@
+type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Innovative Digital Agency",
+            "url": "https://example.com",
+            "logo": "https://example.com/logo.png",
+            "description": "We are a leading digital agency offering premium services in web development, app design, AR/VR, and more.",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "123 Innovation Street",
+                "addressLocality": "Mumbai",
+                "addressRegion": "Maharashtra",
+                "postalCode": "400001",
+                "addressCountry": "IN"
+            },
+            "contactPoint": [
+                {
+                    "@type": "ContactPoint",
+                    "telephone": "+91-12345-67890",
+                    "contactType": "Customer Service",
+                    "areaServed": "IN"
+                }
+            ],
+            "sameAs": [
+                "https://facebook.com/yourpage",
+                "https://twitter.com/yourpage",
+                "https://linkedin.com/company/yourpage",
+                "https://instagram.com/yourpage"
+            ]
+        }
